4e3d1b7eedf26675d693d47ac7e36a112a66ab0e
Guimaraes Neto <guimaraesneto@gmail.com>
2024-06-19 15:07:59 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/winepurchase/api/WinePurchaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.winepurchase.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinePurchaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinePurchaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/winepurchase/api/WinepurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.winepurchase.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinepurchaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
